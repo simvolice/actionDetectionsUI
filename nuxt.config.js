74cf9a653be1282f4f322ebfc1c8a388cbb9c172
simvolice <simvolice@gmail.com>
2021-07-06 20:53:58 +0600

    feat: init

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - actionDetections',
+    title: 'actionDetections',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: "https://code.highcharts.com/highcharts.src.js", body: true, },
+      { src: "https://code.highcharts.com/modules/series-label.js", body: true, },
+      { src: "https://code.highcharts.com/modules/exporting.js", body: true, },
+      { src: "https://code.highcharts.com/modules/export-data.js", body: true, },
+      { src: "https://code.highcharts.com/modules/accessibility.js", body: true, },
+      { src: "https://cdn.jsdelivr.net/npm/moment@2.27.0", body: true, },
+      { src: "https://cdn.jsdelivr.net/npm/moment@2.29.1/locale/ru.js", body: true, },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            Мои камеры
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Поиск"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="camerasArr"
+            :search="search"
+          ></v-data-table>
+        </v-card>
+      </v-col>
+
+    </v-row>
+
+  </v-container>
+
+</template>
+<script>
+export default {
+  data () {
+    return {
+      search: '',
+      headers: [
+        { text: 'Имя камеры', value: 'name' },
+        { text: 'Состояние камеры', value: 'state' },
+        { text: 'Адрес', value: 'address' },
+        { text: 'Дата подключения к системе', value: 'dateCreated' },
+        { text: 'Комментарий', value: 'comment' },
+      ],
+      camerasArr: [
+        {
+          name: 'Камера 1, Подъезд №1',
+          state: 'Работает',
+          address: 'ул. 23-15, 9/1, ЖК "Миллениум Парк"',
+          dateCreated: "25.05.2021",
+          comment: 'Камера установлена у входной группы дома',
+        },
+        {
+          name: 'Камера 2, Подъезд №1',
+          state: 'Работает',
+          address: 'ул. 23-15, 9/1, ЖК "Миллениум Парк"',
+          dateCreated: "05.06.2021",
+          comment: 'Камера установлена в лифте',
+        },
+      ],
+    }
+  },
+}
+</script>
